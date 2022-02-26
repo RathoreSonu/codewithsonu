360fc86cc1b8fbc736412a23dd95f37c8a170501
RathoreSonu <89502429+RathoreSonu@users.noreply.github.com>
2022-02-26 18:02:15 +0530

    Add files via upload

---
+++ hashlib_generate_hash_id.py
@@ -0,0 +1,26 @@
+#it is made by me for easy my work with good quality
+
+import hashlib as lb
+import pandas as pd
+ls=[]
+#load file 
+df=pd.read_excel(r'C:\Users\sonum\OneDrive\Documents\2.Google_Cleaning_Raw_Data\Body_Care_till_18Jan.xlsx')
+#check df
+# print(df)
+# initializing string
+  
+# encoding using encode()
+# then sending to md5()
+for i in df['url']:
+    r= lb.md5(i.encode())
+    p=r.hexdigest()
+#append the values of p in list
+    ls.append(p)
+df['new_hash']=ls
+#check the output
+print(df['new_hash'])
+#store data
+df.to_excel(r'C:\Users\sonum\OneDrive\Documents\2.Google_Cleaning_Raw_Data\1.Body_Care_till_18Jan.xlsx', index=False)
+  
+# printing the equivalent hexadecimal value.
+
